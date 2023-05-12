dca5084bdffa33596b9dd01ae647a6d7bb9893b7
Edgar Hernandez <zdededg@gmail.com>
2023-05-12 13:25:25 -0700

    solved

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+  return a + b;
+}
+function multiply(a, b) {
+    return a * b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  function makeInt(n) {
+    const parsed = parseInt(n, 10);
+    return isNaN(parsed) ? NaN : parsed;
+  }
+  function divide(a, b) {return a/b}
+  function increment(n) {return n + 1}
+  function preserveDecimal(n) {return}
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
